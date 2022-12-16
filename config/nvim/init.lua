23c7b5a686b582ebfd84b4ebc6fb7fdd52625d86
snobee <salexwattson@gmail.com>
2022-12-15 19:22:15 -0800

    Converted init.vim to LuaScript

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,9 @@
+vim.cmd([[
 source $HOME/.config/nvim/general/settings.vim
 source $HOME/.config/nvim/keys/mappings.vim
 
 source $HOME/.config/nvim/vim-plug/plugins.vim
 source $HOME/.config/nvim/plug-config/vimtex.vim
-source $HOME/.config/nvim/plug-config/coc.vim
 source $HOME/.config/nvim/plug-config/supertab.vim
 source $HOME/.config/nvim/plug-config/ultisnips.vim
 source $HOME/.config/nvim/plug-config/airline.vim
@@ -12,3 +12,4 @@ source $HOME/.config/nvim/plug-config/sneak.vim
 source $HOME/.config/nvim/plug-config/gruvbox-material.vim
 source $HOME/.config/nvim/plug-config/hexokinase.vim
 source $HOME/.config/nvim/plug-config/vim-pandoc.vim
+]])
